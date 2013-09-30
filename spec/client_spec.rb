628f6c7b5f541c95285cc5c4ec87c33f345669ce
Eder <eder.esilva@gmail.com>
2013-09-30 15:15:34 -0300

    Fix problem with travis-ci

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,10 +2,8 @@ require 'spec_helper'
 describe "With Client id  and Client Srecret valids" do
   context "Without email and password of user" do
     before do
-    stub_request(:post, "https://api.apontador.com.br/v2/oauth/token").
-      with(:body => {"client_id"=>"12345", "client_secret"=>"54321", "grant_type"=>"client_credentials", "password"=>"", "url"=>"https://api.apontador.com.br/v2/", "username"=>""},
-              :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Faraday v0.8.8'}).
-      to_return(:status => 200, :body => '{"access_token":"token-12345678910","token_type":"bearer","scope":"read trust write"}', :headers => {}) 
+      stub_request(:post, "https://api.apontador.com.br/v2/oauth/token").with(:body => {"client_secret"=>"54321", "username"=>"", "grant_type"=>"client_credentials", "password"=>"", "client_id"=>"12345", "url"=>"https://api.apontador.com.br/v2/"},
+              :headers => {'Accept'=>'*/*', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Faraday v0.8.8'}).to_return(:status => 200, :body => '{"access_token":"token-12345678910","token_type":"bearer","scope":"read trust write"}', :headers => {}) 
       
       #fake_token_without_authentication
       credentials = {:client_id     => "12345", 
@@ -22,10 +20,9 @@ describe "With Client id  and Client Srecret valids" do
   
   context "With email and password valids" do
     before do
-    stub_request(:post, "https://api.apontador.com.br/v2/authenticator/oauth/token").
-         with(:body => {"client_id"=>"12345", "client_secret"=>"54321", "grant_type"=>"password", "password"=>"123456", "url"=>"https://api.apontador.com.br/v2/authenticator", "username"=>"jhondoe@example.com"},
-              :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Faraday v0.8.8'}).
-      to_return(:status => 200, :body => '{"access_token":"token-authenticate-12345678910","token_type":"bearer","scope":"read trust write"}', :headers => {})  
+      stub_request(:post, "https://api.apontador.com.br/v2/authenticator/oauth/token").
+         with(:body => {"client_secret"=>"54321", "username"=>"jhondoe@example.com", "grant_type"=>"password", "password"=>"123456", "client_id"=>"12345", "url"=>"https://api.apontador.com.br/v2/authenticator"},
+              :headers => {'Accept'=>'*/*', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Faraday v0.8.8'}).to_return(:status => 200, :body => '{"access_token":"token-authenticate-12345678910","token_type":"bearer","scope":"read trust write"}', :headers => {})  
       
       #fake_token_with_authentication
       credentials = { :client_id     => "12345",
