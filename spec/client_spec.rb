6603fcbe3ce0429521e8c9213fa69f7f479d0183
Eder <eder.esilva@gmail.com>
2013-09-30 15:23:43 -0300

    Clear setup of test

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 describe "With Client id  and Client Srecret valids" do
   context "Without email and password of user" do
     before do
-      stub_request(:post, "https://api.apontador.com.br/v2/oauth/token").with(:body => {"client_secret"=>"54321", "username"=>"", "grant_type"=>"client_credentials", "password"=>"", "client_id"=>"12345", "url"=>"https://api.apontador.com.br/v2/"},
-              :headers => {'Accept'=>'*/*', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Faraday v0.8.8'}).to_return(:status => 200, :body => '{"access_token":"token-12345678910","token_type":"bearer","scope":"read trust write"}', :headers => {}) 
-      
-      #fake_token_without_authentication
+      fake_token_without_authentication
       credentials = {:client_id     => "12345", 
                      :client_secret => "54321",
                      :url           => "https://api.apontador.com.br/v2/"
@@ -20,11 +17,7 @@ describe "With Client id  and Client Srecret valids" do
   
   context "With email and password valids" do
     before do
-      stub_request(:post, "https://api.apontador.com.br/v2/authenticator/oauth/token").
-         with(:body => {"client_secret"=>"54321", "username"=>"jhondoe@example.com", "grant_type"=>"password", "password"=>"123456", "client_id"=>"12345", "url"=>"https://api.apontador.com.br/v2/authenticator"},
-              :headers => {'Accept'=>'*/*', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Faraday v0.8.8'}).to_return(:status => 200, :body => '{"access_token":"token-authenticate-12345678910","token_type":"bearer","scope":"read trust write"}', :headers => {})  
-      
-      #fake_token_with_authentication
+      fake_token_with_authentication
       credentials = { :client_id     => "12345",
                       :client_secret => "54321",
                       :url           => "https://api.apontador.com.br/v2/authenticator",
